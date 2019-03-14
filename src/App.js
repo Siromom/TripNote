cdcf348f4045bf2a70bc5e31d6d89c7f43ec99c9
Siromom <swon962@gmail.com>
2019-03-14 10:29:51 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, NavLink, HashRouter } from 'react-router-dom';
+import Plan from "./Plan";
+import Buget from "./Buget";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+        <div>
+          <h1>Trip Note</h1>
+          <ul className="header">
+            <li><NavLink to ="/plan">일정</NavLink></li>
+            <li><NavLink to ="/buget">예산</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/plan" component={ Plan }/>
+            <Route exact path="/buget" component={ Buget }/>
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
